58a92eca883554c5fcd11ae7a0f5c91852ac6e51
Jesper Uddström <jesper@uddstrom.se>
2025-12-10 23:55:41 +0100

    day 8 part 1

---
+++ 2025/Day08/main.js
@@ -0,0 +1,79 @@
+import { getData, getPath, min } from '../lib/utils.js';
+
+var PUZZLE_INPUT_PATH = `${getPath(import.meta.url)}/${process.argv[2] ?? 'puzzle_input'}`;
+
+function parser(input) {
+    return input.split('\n').map(row => row.split(',').map(Number));
+}
+
+function straitLineDistance([x1, y1, z1], [x2, y2, z2]) {
+    return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2);
+}
+
+function makeConnections(n) {
+    // find shortest
+    var prev = 0;
+    while (n > 0) {
+        let shortest_distance = Number.MAX_SAFE_INTEGER;
+        let pp;
+        let qq;
+        D.forEach((distances, p) => {
+            let d = min(distances.filter(d => d > prev));
+            if (d > prev && d < shortest_distance) {
+                shortest_distance = d;
+                pp = p;
+                qq = distances.indexOf(d);
+            }
+        });
+        prev = shortest_distance;
+        connect(pp, qq);
+        n--;
+    }
+}
+
+function connect(p, q) {
+    // p and q are in the same cicuite
+    if (C.some(c => c.has(p) && c.has(q))) return;
+
+    var cp = C.findIndex(c => c.has(p));
+    var cq = C.findIndex(c => c.has(q));
+
+    if (cp >= 0 && cq >= 0) {
+        // console.log("Both connected but in different curcuites");
+        let c_merged = new Set([...C[cp], ...C[cq]]);
+        if (cp > cq) {
+            C.splice(cp, 1);
+            C.splice(cq, 1);
+        } else {
+            C.splice(cq, 1);
+            C.splice(cp, 1);
+        }
+        C.push(c_merged);
+    } else if (cp >= 0) {
+        C[cp].add(q);
+    } else if (cq >= 0) {
+        C[cq].add(p);
+    } else {
+        C.push(new Set([p, q]));
+    }
+}
+
+var data = getData(PUZZLE_INPUT_PATH)(parser);
+var D = new Map();
+var C = [];
+
+
+// calculate distances
+data.forEach((p, i) => {
+    D.set(i, data.map((q) => straitLineDistance(p, q)));
+});
+
+makeConnections(1000);
+C.sort((a, b) => b.size - a.size);
+// console.log(C);
+
+var p1 = C.slice(0, 3).reduce((acc, curr) => acc * curr.size, 1);
+
+console.log('Part 1:', p1);
+console.log('Part 2:',);
+
