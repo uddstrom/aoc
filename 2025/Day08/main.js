8bf75e2af32f884d68fa83043218fc5c4cccdb8b
Jesper Uddström <jesper.uddstrom@skandia.se>
2025-12-11 13:52:45 +0100

    day 8

--- 2025/Day08/main.js
+++ 2025/Day08/main.js
@@ -10,36 +10,13 @@ function straitLineDistance([x1, y1, z1], [x2, y2, z2]) {
     return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2);
 }
 
-function makeConnections(n) {
-    // find shortest
-    var prev = 0;
-    while (n > 0) {
-        let shortest_distance = Number.MAX_SAFE_INTEGER;
-        let pp;
-        let qq;
-        D.forEach((distances, p) => {
-            let d = min(distances.filter(d => d > prev));
-            if (d > prev && d < shortest_distance) {
-                shortest_distance = d;
-                pp = p;
-                qq = distances.indexOf(d);
-            }
-        });
-        prev = shortest_distance;
-        connect(pp, qq);
-        n--;
-    }
-}
-
 function connect(p, q) {
-    // p and q are in the same cicuite
     if (C.some(c => c.has(p) && c.has(q))) return;
 
     var cp = C.findIndex(c => c.has(p));
     var cq = C.findIndex(c => c.has(q));
 
     if (cp >= 0 && cq >= 0) {
-        // console.log("Both connected but in different curcuites");
         let c_merged = new Set([...C[cp], ...C[cq]]);
         if (cp > cq) {
             C.splice(cp, 1);
@@ -59,21 +36,41 @@ function connect(p, q) {
 }
 
 var data = getData(PUZZLE_INPUT_PATH)(parser);
-var D = new Map();
-var C = [];
-
+var D = new Map(); // map with distances
+var C = []; // set of circuites
 
 // calculate distances
 data.forEach((p, i) => {
     D.set(i, data.map((q) => straitLineDistance(p, q)));
 });
+D.forEach((value, key) => C.push(new Set([key])));
 
-makeConnections(1000);
-C.sort((a, b) => b.size - a.size);
-// console.log(C);
+var prev = 0;
+var cnt = 0;
+let pp;
+let qq;
+while (C.length > 1) {
+    let shortest_distance = Number.MAX_SAFE_INTEGER;
+    D.forEach((distances, p) => {
+        let d = min(distances.filter(d => d > prev));
+        if (d > prev && d < shortest_distance) {
+            shortest_distance = d;
+            pp = p;
+            qq = distances.indexOf(d);
+        }
+    });
+    prev = shortest_distance;
+    connect(pp, qq);
+    cnt++;
+    if (cnt === 1000) {
+        C.sort((a, b) => b.size - a.size);
+        let p1 = C.slice(0, 3).reduce((acc, curr) => acc * curr.size, 1);
+        console.log('Part 1:', p1);
+    }
+}
 
-var p1 = C.slice(0, 3).reduce((acc, curr) => acc * curr.size, 1);
+var [px, py, pz] = data[pp];
+var [qx, qy, qz] = data[qq];
 
-console.log('Part 1:', p1);
-console.log('Part 2:',);
+console.log('Part 2:', px * qx);
 
