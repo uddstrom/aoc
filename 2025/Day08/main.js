65549863e2fac79af9a96dd33819e71e9ddc74ba
Jesper Uddström <jesper@uddstrom.se>
2025-12-11 17:32:30 +0100

    day 8

--- 2025/Day08/main.js
+++ 2025/Day08/main.js
@@ -1,4 +1,4 @@
-import { getData, getPath, min } from '../lib/utils.js';
+import { getData, getPath, min, product } from '../lib/utils.js';
 
 var PUZZLE_INPUT_PATH = `${getPath(import.meta.url)}/${process.argv[2] ?? 'puzzle_input'}`;
 
@@ -13,37 +13,24 @@ function straitLineDistance([x1, y1, z1], [x2, y2, z2]) {
 function connect(p, q) {
     if (C.some(c => c.has(p) && c.has(q))) return;
 
-    var cp = C.findIndex(c => c.has(p));
-    var cq = C.findIndex(c => c.has(q));
+    var cp = C.find(c => c.has(p));
+    var cq = C.find(c => c.has(q));
 
-    if (cp >= 0 && cq >= 0) {
-        let c_merged = new Set([...C[cp], ...C[cq]]);
-        if (cp > cq) {
-            C.splice(cp, 1);
-            C.splice(cq, 1);
-        } else {
-            C.splice(cq, 1);
-            C.splice(cp, 1);
-        }
-        C.push(c_merged);
-    } else if (cp >= 0) {
-        C[cp].add(q);
-    } else if (cq >= 0) {
-        C[cq].add(p);
+    if (cp && cq) {
+        let c_merged = new Set([...cp, ...cq]);
+        C = [...C.filter(c => !(c.has(p) || c.has(q))), c_merged];
+    } else if (cp) {
+        cp.add(q);
+    } else if (cq) {
+        cq.add(p);
     } else {
         C.push(new Set([p, q]));
     }
 }
 
 var data = getData(PUZZLE_INPUT_PATH)(parser);
-var D = new Map(); // map with distances
-var C = []; // set of circuites
-
-// calculate distances
-data.forEach((p, i) => {
-    D.set(i, data.map((q) => straitLineDistance(p, q)));
-});
-D.forEach((value, key) => C.push(new Set([key])));
+var D = new Map(data.map((p, i) => [i, data.map((q) => straitLineDistance(p, q))]));
+var C = [...D.keys()].map((_, key) => new Set([key])); // set of circuites
 
 var prev = 0;
 var cnt = 0;
@@ -64,13 +51,12 @@ while (C.length > 1) {
     cnt++;
     if (cnt === 1000) {
         C.sort((a, b) => b.size - a.size);
-        let p1 = C.slice(0, 3).reduce((acc, curr) => acc * curr.size, 1);
-        console.log('Part 1:', p1);
+        console.log('Part 1:', product(C.slice(0, 3).map(s => s.size)));
     }
 }
 
-var [px, py, pz] = data[pp];
-var [qx, qy, qz] = data[qq];
+var [px] = data[pp];
+var [qx] = data[qq];
 
 console.log('Part 2:', px * qx);
 
